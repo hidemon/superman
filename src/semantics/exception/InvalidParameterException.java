fe66481dba740c8493ff0e668a0c68d38e38fde5
Yu Sun <yusun@utexas.edu>
2016-10-22 10:19:50 -0500

    exception

---
+++ src/semantics/exception/InvalidParameterException.java
@@ -0,0 +1,7 @@
+package semantics.exception;
+
+public class InvalidParameterException extends Exception {
+
+	private static final long serialVersionUID = 5267991673301147495L;
+
+}
---
+++ src/semantics/exception/NoTokenFoundException.java
@@ -0,0 +1,7 @@
+package semantics.exception;
+
+public class NoTokenFoundException extends Exception {
+	
+	private static final long serialVersionUID = -851650704065124187L;
+
+}
