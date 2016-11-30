138bfa60dd40934c59bb343dc7855f5e5acf8777
lijingjiang <lijingjiangmason@gmail.com>
2016-11-30 16:28:01 -0600

    remove incompatible version

--- SupermanBackEnd/server/publications.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by lijingjiang on 11/13/16 01:27 AM.
- */
-
-Meteor.publish(nlpProcessingsDocument, function() { return NLPProcessings.find(); });
-
-Meteor.publish(cmdExecutionsDocument, function() { return CMDExecutions.find(); });
-
