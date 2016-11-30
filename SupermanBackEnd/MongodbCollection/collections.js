9e794cac24a020ef2b7ae89195e2c4b0459ea54a
lijingjiang <lijingjiangmason@gmail.com>
2016-11-30 16:31:56 -0600

    new storage scheme

---
+++ SupermanBackEnd/MongodbCollection/collections.js
@@ -0,0 +1,10 @@
+/**
+ * Created by lijingjiang on 11/12/16 11:49 PM.
+ */
+
+
+nlpProcessingsDocument = 'nlpprocessings';
+cmdExecutionsDocument = "cmdexecutions";
+
+NLPProcessings = new Mongo.Collection(nlpProcessingsDocument);
+CMDExecutions = new Mongo.Collection(cmdExecutionsDocument);
