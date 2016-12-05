6517ba2496adcf2c2e79cfd7a8781e918c76910b
lijingjiang <lijingjiangmason@gmail.com>
2016-12-05 13:56:34 -0600

    remove deprecated version

--- SupermanBackEnd/client/DataSubscription/subscriptions.js
+++
@@ -1,9 +0,0 @@
-/**
- * Created by lijingjiang on 11/12/16 11:43 PM.
- */
-
-nlpProcessingsDocument= 'nlpprocessings';
-cmdProcessingsDocument= 'cmdprocessings';
-
-Meteor.subscribe(nlpProcessingsDocument);
-Meteor.subscribe(cmdProcessingsDocument);
